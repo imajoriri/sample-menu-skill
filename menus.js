3283d7ee3022b75ddab37a254b9e02846378d556
imajoriri <imajo.srroni@gmail.com>
2018-09-06 18:07:35 +0900

    menus.js追加

---
+++ menus.js
@@ -0,0 +1,26 @@
+exports.breakFirst = [
+  {
+    name: "目玉焼き",
+  },
+  {
+    name: "食パン",
+  }
+]
+
+exports.lunch = [
+  {
+    name: "ラーメン",
+  },
+  {
+    name: "焼きそば",
+  },
+]
+
+exports.dinner = [
+  {
+    name: "カレー",
+  },
+  {
+    name: "オムライス",
+  },
+]
