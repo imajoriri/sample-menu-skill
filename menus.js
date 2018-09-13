529edc51f899aa249acf3f82c814ac3bf1a08eac
imajoriri <imajo.srroni@gmail.com>
2018-09-13 14:44:38 +0900

    メニュー追加

--- menus.js
+++ menus.js
@@ -1,26 +1,57 @@
+
+// 朝
 exports.breakFirst = [
   {
     name: "目玉焼き",
+    image: "https://s3-ap-northeast-1.amazonaws.com/cek-handson/0918/food_image/medamayaki.png",
+    recipe: "https://www.kurashiru.com/recipes/6c830c6d-ec5d-4fb6-87ad-c7e0d6e4b388",
   },
   {
-    name: "食パン",
-  }
+    name: "チージタッカルビおにぎり",
+    image: "https://s3-ap-northeast-1.amazonaws.com/cek-handson/0918/food_image/onigiri.png",
+    recipe: "https://www.kurashiru.com/recipes/79c3492a-ea35-46d3-8db2-e5d98abd9827",
+  },
+  {
+    name: "メロンスムージー",
+    image: "https://s3-ap-northeast-1.amazonaws.com/cek-handson/0918/food_image/smoothie.png",
+    recipe: "https://www.kurashiru.com/recipes/bd43b0b0-fc48-4a93-9950-8f19d9cdde29",
+  },
 ]
 
+// 昼
 exports.lunch = [
   {
-    name: "ラーメン",
+    name: "インスタントラーメンで簡単カルボナーラ",
+    image: "https://s3-ap-northeast-1.amazonaws.com/cek-handson/0918/food_image/carbonara.png",
+    recipe: "https://www.kurashiru.com/recipes/dc8436cf-11cc-48b0-9e5c-0fb48fddf78d",
   },
   {
-    name: "焼きそば",
+    name: "焼きそばナポリタン",
+    image: "https://s3-ap-northeast-1.amazonaws.com/cek-handson/0918/food_image/neapolitan.png",
+    recipe: "https://www.kurashiru.com/recipes/b560015b-8c61-4d2e-bf39-99864bc4a51f",
+  },
+  {
+    name: "豚ヒレカツでふわふわカツ丼",
+    image: "https://s3-ap-northeast-1.amazonaws.com/cek-handson/0918/food_image/katsudon.png",
+    recipe: "https://www.kurashiru.com/recipes/05cf6d99-dc0c-4f44-ac3e-f573f62993b0",
   },
 ]
 
+// 夜
 exports.dinner = [
   {
-    name: "カレー",
+    name: "バターチキンカレー",
+    image: "https://s3-ap-northeast-1.amazonaws.com/cek-handson/0918/food_image/curry.png",
+    recipe: "https://www.kurashiru.com/recipes/d3f2002f-7cd0-4890-964b-703d17422bce",
   },
   {
-    name: "オムライス",
+    name: "きのこのデミグラスソースオムライス",
+    image: "https://s3-ap-northeast-1.amazonaws.com/cek-handson/0918/food_image/omurice.png",
+    recipe: "https://www.kurashiru.com/recipes/6c7ab13f-fac9-4f75-bdba-51628d614c69",
   },
-]
+  {
+    name: "じゃがいもステーキのタルタルソースがけ",
+    image: "https://s3-ap-northeast-1.amazonaws.com/cek-handson/0918/food_image/food_beefsteak.png",
+    recipe: "https://www.kurashiru.com/recipes/90d5ac63-c4d0-4136-bf94-c56e691d51ab",
+  }
+];
